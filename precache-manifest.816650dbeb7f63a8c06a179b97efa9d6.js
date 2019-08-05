b5210ab0085403414508f3f7b654769f8b1d0024
BohdanRadchenko <radchenkobs@gmail.com>
2019-08-05 13:39:25 +0300

    Updates

---
+++ precache-manifest.816650dbeb7f63a8c06a179b97efa9d6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd0a7f50274f515241807d7c3ecd3d31",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "0c55d208564132cf581f",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.400bbf32.chunk.js"
+  },
+  {
+    "revision": "052338c15f0cfa810fb3",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime~main.a33a0e9d.js"
+  }
+]);
