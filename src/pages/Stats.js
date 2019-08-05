b0bf6ddbd704c615a12cfa2776b26a3860fd6290
BohdanRadchenko <radchenkobs@gmail.com>
2019-08-05 16:17:53 +0300

    added stats

--- src/pages/Stats.js
+++ src/pages/Stats.js
@@ -1,11 +1,39 @@
+/*eslint-disable*/
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import StatsDiagram from '../components/Stats/StatsDiagram/StatsDiagram';
 import StatsTable from '../components/Stats/StatsTable/StatsTable';
+import db from '../db.json';
+
+const stateSum = items => {
+  const depositsArr = items.filter(el => el.type === 'Deposits');
+  const depositsSumm = depositsArr.reduce(
+    (acc, el) => (acc += el.inputValue),
+    0,
+  );
+  const withdrowArr = items.filter(el => el.type === 'Withdrow');
+  const withdrowSumm = withdrowArr.reduce(
+    (acc, el) => (acc += el.inputValue),
+    0,
+  );
+  const newBalance = depositsSumm - withdrowSumm;
+
+  const stateObj = {
+    balance: newBalance,
+    deposits: depositsSumm,
+    withdrow: withdrowSumm,
+  };
+  return stateObj;
+};
 
 class Stats extends Component {
-  state = {};
+  state = {
+    transactions: [],
+    items: db,
+  };
 
   render() {
+    console.log(this.props);
     return (
       <div>
         <StatsDiagram />
@@ -15,4 +43,11 @@ class Stats extends Component {
   }
 }
 
-export default Stats;
+const mapStateToProps = state => ({
+  transactions: db,
+});
+
+export default connect(
+  mapStateToProps,
+  null,
+)(Stats);
