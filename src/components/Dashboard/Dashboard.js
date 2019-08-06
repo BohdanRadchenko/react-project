bdc62f69545f8d61344bf70b44f03825527e01e5
BohdanRadchenko <radchenkobs@gmail.com>
2019-08-06 08:26:02 +0300

    added Loader

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
+import Loadable from 'react-loadable';
 import SignUpPage from '../../pages/SignUpPage';
-import Home from '../../pages/Home';
-import Stats from '../../pages/Stats';
 import SignInPage from '../../pages/SignInPage';
+import Loader from './Loader';
+
+const AsyncHome = Loadable({
+  loader: () => import('../../pages/Home' /* webpackChunkName: "home-page" */),
+  loading: Loader,
+  timeout: 10000,
+  delay: 200,
+});
+
+const AsyncStats = Loadable({
+  loader: () => import('../../pages/Stats' /* webpackChunkName: "home-page" */),
+  loading: Loader,
+  timeout: 10000,
+  delay: 200,
+});
 
 class Dashboard extends Component {
   state = {};
@@ -11,10 +25,10 @@ class Dashboard extends Component {
   render() {
     return (
       <Switch>
-        <Route path="/" exact component={Home} />
+        <Route path="/" exact component={AsyncHome} />
         <Route path="/signup" component={SignUpPage} />
         <Route path="/signin" component={SignInPage} />
-        <Route path="/stats" component={Stats} />
+        <Route path="/stats" component={AsyncStats} />
         <Redirect to="/" />
       </Switch>
     );
