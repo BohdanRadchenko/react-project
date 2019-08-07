974574f4221a9c780bf7c6eabe01a3e215ebda27
BohdanRadchenko <radchenkobs@gmail.com>
2019-08-07 08:20:31 +0300

    added propTypes

--- src/components/Stats/StatsDiagram/StatsDiagram.js
+++ src/components/Stats/StatsDiagram/StatsDiagram.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const StatsDiagram = () => (
-  <div>
-    <p>Stats Diagramm</p>
-  </div>
-);
+const StatsDiagram = ({ items }) => {
+  return (
+    <ul>
+      {items.map(el => (
+        <li key={el.id}>
+          <p>{el.amount}</p>
+          <p>{el.balanceAfter}</p>
+          <p>{el.category}</p>
+          <p>{el.comments}</p>
+          <p>{el.data}</p>
+          <p>{el.isAmountNull}</p>
+          <p>{el.type}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+StatsDiagram.defaultProps = {
+  items: [],
+};
+
+StatsDiagram.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      amount: PropTypes.number,
+      balanceAfter: PropTypes.number,
+      category: PropTypes.string,
+      comments: PropTypes.string,
+      data: PropTypes.string,
+      isAmountNull: PropTypes.bool,
+      type: PropTypes.string,
+    }),
+  ),
+};
 
 export default StatsDiagram;
--- src/components/Stats/StatsTable/StatsTable.js
+++ src/components/Stats/StatsTable/StatsTable.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const StatsTable = () => (
-  <div>
-    <p>Stats Table</p>
-  </div>
-);
+const StatsTable = ({ items }) => {
+  return (
+    <ul>
+      {items.map(el => (
+        <li key={el.id}>
+          <p>{el.amount}</p>
+          <p>{el.balanceAfter}</p>
+          <p>{el.category}</p>
+          <p>{el.comments}</p>
+          <p>{el.data}</p>
+          <p>{el.isAmountNull}</p>
+          <p>{el.type}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+StatsTable.defaultProps = {
+  items: [],
+};
+
+StatsTable.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      amount: PropTypes.number,
+      balanceAfter: PropTypes.number,
+      category: PropTypes.string,
+      comments: PropTypes.string,
+      data: PropTypes.string,
+      isAmountNull: PropTypes.bool,
+      type: PropTypes.string,
+    }),
+  ),
+};
 
 export default StatsTable;
