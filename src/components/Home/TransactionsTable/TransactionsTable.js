3161a11ee03e357f5111d0d8a60405a61e4c1457
Anton Azhhirevych <azhhirevych943@gmail.com>
2019-08-08 13:50:35 +0300

    added function get month

--- src/components/Home/TransactionsTable/TransactionsTable.js
+++ src/components/Home/TransactionsTable/TransactionsTable.js
@@ -11,12 +11,30 @@ import styles from './TransactionsTable.module.css';
 
 const getMonth = month => {
   switch (month) {
-    case 'May':
+    case 'January':
       return '01';
-    case month === 'sfsf':
+    case 'February':
       return '02';
-    case month === 'fsfs':
+    case 'March':
       return '03';
+    case 'April':
+      return '04';
+    case 'May':
+      return '05';
+    case 'June':
+      return '06';
+    case 'July':
+      return '07';
+    case 'August':
+      return '08';
+    case 'September':
+      return '09';
+    case 'October':
+      return '10';
+    case 'November':
+      return '11';
+    case 'December':
+      return '12';
     default:
       return 'Hello';
   }
