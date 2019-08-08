8826d3f891095bacd53091c4e78cc48af7c0a5c8
BohdanRadchenko <radchenkobs@gmail.com>
2019-08-08 11:28:35 +0300

    added Currencies component/ fix

--- src/components/Dashboard/Sidebar/Sidebar.js
+++ src/components/Dashboard/Sidebar/Sidebar.js
@@ -2,12 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Navigations from '../Navigations/Navigations';
 import Balance from '../Balance/Balance';
+import Currencies from '../Currencies/Currencies';
 
 const Sidebar = ({ balance }) => (
   <div>
     <Navigations />
     <Balance balance={balance} />
-    {/* <Currencies /> */}
+    <Currencies />
   </div>
 );
 
