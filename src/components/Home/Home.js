fb0d0f45de4b23f0a2bcc5404aa4f739704dca52
Andrein <nykolynandrii@gmail.com>
2019-08-09 13:33:23 +0300

    adds anim to transaction

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,14 @@
 /*eslint-disable*/
 import React, { Component } from 'react';
+import Media from 'react-media';
+import { CSSTransition } from 'react-transition-group';
 import { connect } from 'react-redux';
 import TransactionsTable from './TransactionsTable/TransactionsTable';
 import AddButton from './AddButton/AddButton';
 import Modal from '../Modal/ModalContainer';
 import styles from './Home.module.css';
 import Welcome from './Welcome/Welcome';
+import SlideTransition from './Slide.module.css';
 
 class Home extends Component {
   state = {
@@ -33,7 +36,24 @@ class Home extends Component {
           {finance.length !== 0 && <TransactionsTable items={arr} />}
           <AddButton onOpen={this.handleOpen} />
         </div>
-        {isOpenModal && <Modal onClose={this.handleClose} />}
+        <Media
+          query="(max-width: 766px)"
+          render={() => (
+            <CSSTransition
+              in={isOpenModal}
+              timeout={200}
+              classNames={SlideTransition}
+              unmountOnExit
+            >
+              <Modal onClose={this.handleClose} />
+            </CSSTransition>
+          )}
+        />
+        <Media
+          query="(min-width: 767px)"
+          render={() => isOpenModal && <Modal onClose={this.handleClose} />}
+        />
+        {/* {isOpenModal && <Modal onClose={this.handleClose} />} */}
       </div>
     );
   }
